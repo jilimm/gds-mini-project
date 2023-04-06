dba2b92a57ed3a6b6a92c61f0a5c6e63ff2e3c0a
limjy <25588260+limjyy@users.noreply.github.com>
2023-04-07 01:58:45 +0800

    code clean up

--- src/main/java/com/gds/challenge/utils/validators/MaxMoreThanOrEqualToMin.java
+++ src/main/java/com/gds/challenge/utils/validators/MaxMoreThanOrEqualToMin.java
@@ -3,14 +3,16 @@ package com.gds.challenge.utils.validators;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.CONSTRUCTOR;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Constraint(validatedBy = MaxMoreThanOrEqualToMinValidator.class)
-@Target({ METHOD, CONSTRUCTOR })
+@Target({METHOD, CONSTRUCTOR})
 @Retention(RUNTIME)
 @Documented
 public @interface MaxMoreThanOrEqualToMin {
